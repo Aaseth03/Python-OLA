b25edb5ac274b1a0bc455c215cb5637f4da77bd4
Ultralyd <christoffer.aaseth@gmail.com>
2024-08-31 13:11:15 +0200

    Challange1

---
+++ Challange-1/Funksjoner.py
@@ -0,0 +1,25 @@
+"""
+Oppg.1 *
+Lag en funksjon som konverterer grader til radianer.
+**Bonus om svaret er uttrykt som ne brøk med pi**
+"""
+
+
+
+
+
+"""
+Oppg.2 **
+Lag en funksjon som regner ut arealet av en trapes.
+Funksjonen skal ha inn parameterene a,b,h. h er høyden, a og b er lengdene på sidekantene
+"""
+
+
+
+
+
+"""
+Oppg.3 ***
+Lag en funksjon som faktoriserer en 2.grads likning ved bruk av ABC-formelen.
+Funksjonen tar inn argumentene a,b,c. Når y = a*x^2 + b*x + c => (x - x_1)(x - x_2)
+"""
---
+++ Challange-1/Hint1.py
@@ -0,0 +1,9 @@
+
+#Generell funksjon syntax(struktur/skrivemåte)
+
+def Funksjon_navn(Parameter1,Parameter2):
+    #Hva skal funksjonen gjøre?
+    #f.eks
+    Ut_Verdi = Parameter1 + Parameter2
+    return Ut_Verdi
+
