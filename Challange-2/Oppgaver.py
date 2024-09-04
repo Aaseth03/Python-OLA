6a76059929ae07f4cfa38a4387481f9c5d095d53
Aaseth <christoffer.aaseth@gmail.com>
2024-09-04 17:58:52 +0200

    Challange-2 opprettet

---
+++ Challange-2/Oppgaver.py
@@ -0,0 +1,39 @@
+"""
+oppg. 1
+Hvilken type telefon har Thea?
+Bruk register.py som en modul og hent ut informasjonen i en annen fil.
+Ikke se i register.py, bruk coden under for å begynne"
+
+Hint: Telefon typen ligger skrevet under "TLF"
+"""
+import register
+
+print(register.Klasseliste)
+
+
+"""
+oppg.2
+
+Lag et program som bruker ei for-løkke til å summere de N første 2-er
+potensene, dvs summen:
+2^1 + 2^2 + 2^3+ . . . +2^N
+
+(N>0 er et positivt heltall, for eksempel kan N=28, og det siste tallet i summen
+blir da 2^28).
+"""
+
+
+"""
+oppg.3
+
+Lag et program som bruker ei while-løkke til å summere de N første 2-er
+potensene, dvs summen:
+2^1 + 2^2 + 2^3 + . . . +2^N
+
+Hvor mange itterasjoner trengs for at summen er over 8000? Hva blir summen?
+(N>0 er et positivt heltall, for eksempel kan N=28, og det siste tallet i summen
+blir da 2^28).
+"""
+
+
+
---
+++ Challange-2/register.py
@@ -0,0 +1,53 @@
+
+import numpy as np
+
+Klasseliste = np.array(["Petter","Anna","Peder","Rikke","Martin","Sofie","Thea","Markus"])
+
+np.random.shuffle(Klasseliste)
+
+Petter = {
+    "Navn"      :   "Petter",
+    "TLF"       :   "Android",
+    "StudentID" :   "239045"
+}
+
+Anna = {
+    "Navn"      :   "Anna",
+    "TLF"       :   "iOS",
+    "StudentID" : "255039"
+}
+
+Peder = {
+    "Navn"      :   "Peder",
+    "TLF"       :   "Android",
+    "StudentID" :   "238025"
+}
+
+Rikke = {
+    "Navn"      :   "Rikke",
+    "TLF"       :   "iOS",
+    "StudentID" : "255028"
+}
+Martin = {
+    "Navn"      :   "Martin",
+    "TLF"       :   "Android",
+    "StudentID" : "252569"
+}
+
+Sofie = {
+    "Navn"      :   "Sofie",
+    "TLF"       :   "Android",
+    "StudentID" :   "234832"
+}
+
+Thea = {
+    "Navn"      :   "Thea",
+    "TLF"       :   "Android",
+    "StudentID" : "204852"
+}
+
+Markus = {
+    "Navn"      :   "Markus",
+    "TLF"       :   "iOS",
+    "StudentID" :   "284839"
+}
